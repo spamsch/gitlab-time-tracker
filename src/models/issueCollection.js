e85580bcbbb9f6d391503bf0f704e14663973e5a
Simon Pamies <spamsch@gmail.com>
2018-03-01 12:59:08 +0100

    Ensure that assignee is transferred

--- src/models/issueCollection.js
+++ src/models/issueCollection.js
@@ -7,23 +7,22 @@ class issueCollection extends Base {
 	constructor(config) {
 		super(config);
 
-		this._config = config;
-		this._issues = [];
+		this.config = config;
+		this.issues = [];
 
 		this._state = `opened`;
 		this._scope = `assigned-to-me`;
-
 	}
 
 	all(username, state = this._state, scope = this._scope) {
 		return new Promise((resolve, reject) => {
-			super.all(`issues?state=${this._state}&scope=${this._scope}`)
+			super.all(`issues?state=${this._state}&scope=${this._scope}&assignee_id=${username}`)
 				.then((issues_collected) => {
 					_.each(issues_collected, (issue_raw) => {
-						let issue = new Issue(this._config, issue_raw);
-						this._issues.push(issue);
+						let issue = new Issue(this.config, issue_raw);
+						this.issues.push(issue);
 					});
-					resolve(this._issues);
+					resolve(this.issues);
 				});
 		});
 	}
